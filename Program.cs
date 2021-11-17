81a8b308008710e84b03492c6854cad46c9f986c
gabriel.gutierrez <gabriel.gutierrez@agilethought.com>
2021-11-17 17:39:47 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Advanced_Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
