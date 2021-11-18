61a194c7a632af08ae1ec5f9d4c64d57fea25b49
gabriel.gutierrez <gabriel.gutierrez@agilethought.com>
2021-11-17 18:53:09 -0600

    Lecture2

---
+++ Person.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_Course
+{
+    
+    class Person
+    {
+        private string name;
+        private int age;
+        private bool canWalk;
+
+        public Person(string name) {
+            this.name = name;
+        }
+
+        
+        public int Age{
+            set {
+                
+                if (value is 0) {
+                    throw new ArgumentException("Age Cannot be zero");
+                }
+                if (value < 2 || value > 89)
+                    canWalk = false;
+                else
+                    canWalk = true;
+            }
+            get => age;
+            
+        }
+
+        public Boolean Equal(Person param)
+        {
+            try
+            {
+                if (param.GetType() == typeof(Person))
+                {
+                    if (param.Age.ToString() == param.name)
+                    {
+                        return (true);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+                return (false);
+            }
+            return (false);
+        }
+
+    }
+}
